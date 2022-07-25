ed8b164154632f02faf65c607d4a9c4d44a19bbc
Start579 <103378949+Start579@users.noreply.github.com>
2022-07-25 08:03:25 +0000

    Deploying to gh-pages from @ Start579/group@4de8847d0c6bb318bcaf503d4da2c721cdfdbb41 🚀

---
+++ index.5f764e19.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}(),document.querySelector(".js-speaker-form").addEventListener("submit",(function(e){e.preventDefault(),new FormData(e.currentTarget).forEach((function(e,o){return console.log("".concat(o,": ").concat(e))}))}));
+//# sourceMappingURL=index.5f764e19.js.map
